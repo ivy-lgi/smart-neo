67217cfacc5d9fde611fbae5c1241b71ededba0c
Lukas Giger <lukas.giger@axonivy.com>
2025-07-14 09:08:48 +0200

    AIAI-51 add test bundle

---
+++ smart-neo-test/test/com/axonivy/smart/neo/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package com.axonivy.smart.neo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHelloWorld {
+
+  @Test
+  void testHelloWorld() {
+    assertThat("Hello, World!").isEqualTo("Hello, World!");
+  }
+}
